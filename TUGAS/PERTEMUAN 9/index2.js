1bcf8bdf4f32dd125546981f69a9f35009fe3a25
ana <amust93@gmail.com>
2019-11-10 15:47:28 +0700

    [ana} -  tambahan

--- TUGAS/PERTEMUAN 9/index2.js
+++ TUGAS/PERTEMUAN 9/index2.js
@@ -1,22 +1,42 @@
 const fs = require('fs');
 
 
-/* create new file
+//create new file
+//let teks = 'This is new file'
+//fs.writeFile('newfile.txt', teks, (err) => {
+//  if (err) throw err;
+//  console.log('Succesfully created new file')
+//});
+//
+
+//read file that we've created at the previous code
+//fs.readFile(__dirname + '/newfile.txt', 'utf-8', (err, data) => {
+//   if (err) throw err;
+//  console.log(data);
+//});
+
+//console.log('test')
+
+//TUGAS
+// create directory
+//let fs = require('fs');
+//let dir = './ana';
+//  fs.mkdirSync(dir);
+
+// create file in directory
+
 let teks = 'This is new file'
-fs.writeFile('newfile.txt', teks, (err) => {
+fs.writeFile('./ana/newfile.txt', teks, (err) => {
     if (err) throw err;
     console.log('Succesfully created new file')
 });
-*/
 
-//read file that we've created at the previous code
-fs.readFile(__dirname + '/newfile.txt', 'utf-8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-});
 
-console.log('test')
 
-TUGAS
-// create directory
-// create file in directory
+
+//var fs = require('fs');
+//var dir = './tmp';
+
+//if (!fs.existsSync(dir)){
+//   fs.mkdirSync(dir);
+//}
